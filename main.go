f3c323c135a70b89d7921b882c96136759512aa0
Ben Shaharizad <bnsd55@gmail.com>
2016-11-24 22:51:27 +0200

    pg test

--- main.go
+++ main.go
@@ -5,9 +5,36 @@ import (
 	"net/http"
 	"os"
 	"io"
+	"gopkg.in/pg.v5"
+	"fmt"
 )
 
+type User struct {
+	Id     int64
+	Name   string
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("User<%d %s>", u.Id, u.Name)
+}
+
 func index (res http.ResponseWriter, req *http.Request) {
+	db := pg.Connect(&pg.Options{
+		User:     "bzvpyynezpuruw",
+		Database: "derkj04fmn4ff8",
+		Password: "xpyHRQ1ScLn0ZCGqsJLxOficyO",
+		Addr:     "54.75.230.123:5432",
+	})
+
+	user1 := &User{
+		Name:   "admin",
+	}
+
+	err := db.Insert(user1)
+	if err != nil {
+		panic(err)
+	}
+
 	io.WriteString(res, "foo ran")
 }
 
