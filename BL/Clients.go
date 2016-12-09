bd651a03d6173605729e24ce206b43f676c16f03
Ben Shaharizad <bnsd55@gmail.com>
2016-12-09 17:17:01 +0200

    debug

--- BL/Clients.go
+++ BL/Clients.go
@@ -18,7 +18,7 @@ func CreateClient(w http.ResponseWriter, r *http.Request, params httprouter.Para
 	err := json.NewDecoder(r.Body).Decode(&client)
 
 	if err != nil {
-		log.Panic(err)
+		//log.Panic(err)
 	}
 
 	if govalidator.IsNumeric(client.PhoneNumber) {
